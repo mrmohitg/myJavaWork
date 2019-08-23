d43603e556078a576a19eaa2fd8082995e8d2f09
Mohit <mohit11187@gmail.com>
2019-08-23 15:51:35 +0530

    Pointcuts and Wildcards

--- SpringBasics/src/learning/spring/aop/aspect/LoggingAspect.java
+++ SpringBasics/src/learning/spring/aop/aspect/LoggingAspect.java
@@ -1,15 +1,34 @@
 package learning.spring.aop.aspect;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
-	@Before("execution(public String getName())")
+	@Before("execution(public String learning.spring.aop.model.Circle.getName())")
 	public void loggingAdvice()
 	{
 		System.out.println("Advice Run and Get method is called.");
 	}
+	
+	
+	@Before("execution(public * get*(..))")
+	public void loggingAdvice1()
+	{
+		System.out.println("All getter Wild card");
+	}
+	
+	@After("allGetters()")
+	public void loggingAdvice2()
+	{
+		System.out.println("All getter Wild card");
+	}
+	
+	@Pointcut("execution(public * get*(..))")
+	public void allGetters() {}
+	
 			
 }
