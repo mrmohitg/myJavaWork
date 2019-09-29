baa452fcda9d38964a2ed753fa2f333b03eaf46b
Mohit <mr.mohit.gupta.gi@gmail.com>
2019-09-29 17:39:10 +0530

    Overriding delete method save method and changing repositoryRestResource

--- explorecali/src/main/java/com/example/ec/explorecali/repository/TourPackageRepository.java
+++ explorecali/src/main/java/com/example/ec/explorecali/repository/TourPackageRepository.java
@@ -4,14 +4,53 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.ec.explorecali.domain.Tour;
 import com.example.ec.explorecali.domain.TourPackage;
 
+@RepositoryRestResource(collectionResourceRel = "packages", path="packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String>{
 
 	public TourPackage findByName(@Param("name") String name);
 	
 	public List<Tour> findByCode(String code);
+
+	/*@Override
+	@RestResource(exported=false)
+	default void delete(TourPackage arg0) {
+		
+	}
+
+	@Override
+	@RestResource(exported=false)
+	default void deleteAll() {
+		
+	}
+
+	@Override
+	@RestResource(exported=false)
+	default void deleteAll(Iterable<? extends TourPackage> arg0) {
+		
+	}
+
+	@Override
+	@RestResource(exported=false)
+	default void deleteById(String arg0) {
+		
+	}
+
+	@Override
+	@RestResource(exported=false)
+	default <S extends TourPackage> S save(S arg0) {
+		return null;
+	}
+
+	@Override
+	@RestResource(exported=false)
+	default <S extends TourPackage> Iterable<S> saveAll(Iterable<S> arg0) {
+		return null;
+	}*/
 	
 }
--- explorecali/src/main/java/com/example/ec/explorecali/repository/TourRepository.java
+++ explorecali/src/main/java/com/example/ec/explorecali/repository/TourRepository.java
@@ -3,15 +3,22 @@ package com.example.ec.explorecali.repository;
 import java.util.Collection;
 import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.ec.explorecali.domain.Difficulty;
 import com.example.ec.explorecali.domain.Region;
 import com.example.ec.explorecali.domain.Tour;
 
-public interface TourRepository extends CrudRepository<Tour, Integer>{
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer>{
 	
-	public List<Tour> findByPrice(int price);
+	public List<Tour> findByPrice(Integer price);
 	
 	public Collection<Tour> findByDifficulty(Difficulty difficulty);
 	
@@ -29,6 +36,44 @@ public interface TourRepository extends CrudRepository<Tour, Integer>{
 	
 	public List<Tour> findByTourPackageCodeAndDifficultyAndRegionAndPriceLessThan(String code, Difficulty difficulty, Region region,Integer maxPrice);
 	
-	public List<Tour> findByTourPackageCode(String code);
+	public Page<Tour> findByTourPackageCode(@Param("code")String code, Pageable pageable);
+
+	/*@Override
+	@RestResource(exported=false)
+	default void delete(Tour arg0) {
+		
+	}
+
+	@Override
+	@RestResource(exported=false)
+	default void deleteAll() {
+		
+	}
+
+	@Override
+	@RestResource(exported=false)
+	default void deleteAll(Iterable<? extends Tour> arg0) {
+		
+	}
+
+	@Override
+	@RestResource(exported=false)
+	default void deleteById(Integer arg0) {
+		
+	}
+
+	@Override
+	@RestResource(exported=false)
+	default <S extends Tour> S save(S arg0) {
+		return null;
+	}
+
+	@Override
+	@RestResource(exported=false)
+	default <S extends Tour> Iterable<S> saveAll(Iterable<S> arg0) {
+		return null;
+	}*/
+	
+	
 	
 }
