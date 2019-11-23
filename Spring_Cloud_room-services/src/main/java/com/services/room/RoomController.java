972acc9ef10014d3d5dfb97461c44a425e1a0993
Mohit <mr.mohit.gupta.gi@gmail.com>
2019-11-23 23:26:41 +0530

    Swagger Implementation

--- Spring_Cloud_room-services/src/main/java/com/services/room/RoomController.java
+++ Spring_Cloud_room-services/src/main/java/com/services/room/RoomController.java
@@ -10,13 +10,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@Api(value="rooms", description="Data Service Operations on Rooms", tags="rooms")
 @RequestMapping("/room")
 public class RoomController {
 
 	@Autowired
 	private RoomRepository roomRepository;
 
+	@ApiOperation(value="Get All Rooms", notes="Get all the rooms in the system", nickname="getRooms")
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Room> findAll(@RequestParam(name = "roomNumber", required = false) String roomNumber) {
 		if (StringUtils.isNotEmpty(roomNumber)) {
--- Spring_Cloud_room-services/src/main/java/com/services/room/RoomServicesApplication.java
+++ Spring_Cloud_room-services/src/main/java/com/services/room/RoomServicesApplication.java
@@ -1,11 +1,33 @@
 package com.services.room;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.any;
 
 @SpringBootApplication
+@EnableSwagger2
 public class RoomServicesApplication {
 
+	@Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2).groupName("Room").select()
+                .apis(RequestHandlerSelectors.basePackage("com.services.room"))
+                .paths(any()).build().apiInfo(new ApiInfo("Room Services",
+                        "A set of services to provide data access to rooms", "1.0.0", null,
+                        new Contact("Mohit", "https://www.google.com", null),null, null));
+    }
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RoomServicesApplication.class, args);
 	}
