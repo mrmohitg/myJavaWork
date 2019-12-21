cca73cce0960d41904de3d7f55167ad3de1b8843
Mohit <mr.mohit.gupta.gi@gmail.com>
2019-12-21 19:52:59 +0530

    Angular + Spring Boot

---
+++ linked-in-learning-full-stack-app-angular-spring-boot/src/main/java/com/linkedin/learning/linkedinlearningfullstackappangularspringboot/LinkedInLearningFullStackAppAngularSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.linkedin.learning.linkedinlearningfullstackappangularspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedInLearningFullStackAppAngularSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedInLearningFullStackAppAngularSpringBootApplication.class, args);
+	}
+
+}
